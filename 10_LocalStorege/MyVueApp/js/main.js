585d569a48e26802281d96cda8f252cf7393ef76
Takashi <takashi-going-sky@hotmail.co.jp>
2020-04-12 01:02:46 +0900

    LocalStorege

---
+++ 10_LocalStorege/MyVueApp/js/main.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: []
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        }
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index){
+        if (confirm('削除してよろしいですか?')) {
+          this.todos.splice(index, 1);
+        }
+      },
+      purge: function(){
+        if (!confirm('削除してよろしいですか?')){
+          return;
+        }
+        this.todos = this.remaining;
+      }
+    },
+    watch: {
+      todos: {
+        handler: function(){
+          localStorage.setItem("todos", JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+    mounted:function(){
+      this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+    },
+    computed: {
+      remaining: function(){
+        return this.todos.filter(function(todo){
+          return !todo.isDone;
+        });
+      }
+    }
+  });
+})();
